01afc12cbad58fd416bc10743a311d35fb468abc
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-02-24 20:23:53 +0545

    Refactor connection checks and improve HTML formatting in user registration form

--- connect.php
+++ connect.php
@@ -3,7 +3,6 @@ $servername="localhost";
 $username="root";
 $password="";
 $conn=mysqli_connect($servername,$username,$password);
-//check connection
 if($conn){
         die("Connection Failed:".mysqli_connect_error());
 }
